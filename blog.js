5d41dd42e48ca4ab2d551ae6b5d3053a6088eebd
Alan Lam <all123@ucsd.edu>
2018-05-01 12:18:34 -0700

    update

--- blog.js
+++ blog.js
@@ -1,16 +1,3 @@
-function beginning() {
-    var x = document.getElementById("more_beginning");
-    var b = document.getElementById("more_beginning_button");
-    if (x.style.display === "none" || x.style.display == "") {
-        x.style.display = "block";
-        b.style.display = "none";
-    }
-    else {
-        x.style.display = "none";
-        b.style.display = "inline";
-    }
-}
-
 function functions() {
     var x = document.getElementById("more_function");
     var b = document.getElementById("more_function_button");
