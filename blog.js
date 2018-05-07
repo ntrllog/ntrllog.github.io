6472d14c5af71984c0ae7c6eaf99f210f9ff1e65
Alan Lam <all123@ucsd.edu>
2018-05-07 11:36:54 -0700

    update

--- blog.js
+++ blog.js
@@ -1,51 +1,51 @@
 function functions() {
-    var x = document.getElementById("more_function");
-    var b = document.getElementById("more_function_button");
-    if (x.style.display === "none" || x.style.display == "") {
-        x.style.display = "block";
-        b.style.display = "none";
-    }
-    else {
-        x.style.display = "none";
-        b.style.display = "inline";
-    }
+  var x = document.getElementById("more_function");
+  var b = document.getElementById("more_function_button");
+  if (x.style.display === "none" || x.style.display == "") {
+    x.style.display = "block";
+    b.style.display = "none";
+  }
+  else {
+    x.style.display = "none";
+    b.style.display = "inline";
+  }
 }
 
 function hsCulture() {
-    var x = document.getElementById("more_hs_culture");
-    var b = document.getElementById("more_hs_culture_button");
-    if (x.style.display === "none" || x.style.display == "") {
-        x.style.display = "block";
-        b.style.display = "none";
-    }
-    else {
-        x.style.display = "none";
-        b.style.display = "inline";
-    }
+  var x = document.getElementById("more_hs_culture");
+  var b = document.getElementById("more_hs_culture_button");
+  if (x.style.display === "none" || x.style.display == "") {
+    x.style.display = "block";
+    b.style.display = "none";
+  }
+  else {
+    x.style.display = "none";
+    b.style.display = "inline";
+  }
 }
 
 function learningMath() {
-    var x = document.getElementById("more_learning_math");
-    var b = document.getElementById("more_learning_math_button");
-    if (x.style.display === "none" || x.style.display == "") {
-        x.style.display = "block";
-        b.style.display = "none";
-    }
-    else {
-        x.style.display = "none";
-        b.style.display = "inline";
-    }   
+  var x = document.getElementById("more_learning_math");
+  var b = document.getElementById("more_learning_math_button");
+  if (x.style.display === "none" || x.style.display == "") {
+    x.style.display = "block";
+    b.style.display = "none";
+  }
+  else {
+    x.style.display = "none";
+    b.style.display = "inline";
+  }   
 }
 
 function haircut() {
-    var x = document.getElementById("more_haircut");
-    var b = document.getElementById("more_haircut_button");
-    if (x.style.display === "none" || x.style.display == "") {
-        x.style.display = "block";
-        b.style.display = "none";
-    }
-    else {
-        x.style.display = "none";
-        b.style.display = "inline";
-    }   
+  var x = document.getElementById("more_haircut");
+  var b = document.getElementById("more_haircut_button");
+  if (x.style.display === "none" || x.style.display == "") {
+    x.style.display = "block";
+    b.style.display = "none";
+  }
+  else {
+    x.style.display = "none";
+    b.style.display = "inline";
+  }   
 }
--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 function displayAnswer() {
-    var text;
-    var guess = prompt("There's likely a better way to ask for a guess");
-    if (guess == "112114") {
-      text = "Yep";
-    }
-    else {
-      text = "Nope";
-    }
-    document.getElementById("answer").innerHTML = text;
+  var text;
+  var guess = prompt("There's likely a better way to ask for a guess");
+  if (guess == "112114") {
+    text = "Yep";
   }
+  else {
+    text = "Nope";
+  }
+  document.getElementById("answer").innerHTML = text;
+}
